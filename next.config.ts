ed0719835160bd98255c5b0e10a90e8dfd238f1e
asdhashdh <152227472+asdhashdh@users.noreply.github.com>
2025-04-07 17:13:14 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // If your site will be in a subdirectory (like username.github.io/repo-name)
-  // uncomment and update the following line:
-  // basePath: '/your-repo-name',
 }
 
 export default nextConfig
