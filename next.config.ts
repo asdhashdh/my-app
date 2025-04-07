fe85ac4fb55a54fbf6f78c6c846bf241a84f161d
asdhashdh <152227472+asdhashdh@users.noreply.github.com>
2025-04-07 17:07:10 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your site will be in a subdirectory (like username.github.io/repo-name)
+  // uncomment and update the following line:
+  // basePath: '/your-repo-name',
+}
 
-export default nextConfig;
+export default nextConfig
